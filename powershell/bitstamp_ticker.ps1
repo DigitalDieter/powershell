14b7631dea1931badd42bcb45545a5a54614e31f
Michael <m.mueller444@gmail.com>
2019-06-11 05:23:46 +0200

    extended function

---
+++ powershell/bitstamp_ticker.ps1
@@ -0,0 +1,15 @@
+﻿
+Function bitstamp_ticker  {
+
+    $Site = "https://www.bitstamp.net/api/v2/ticker/btcusd/"
+    $ticker_price =  Invoke-RestMethod -Uri $Site |ForEach-Object {$_.ask}
+    write-host $ticker_price
+}
+do
+{
+    bitstamp_ticker
+    start-sleep -Seconds 2
+
+}until($infinity)
+
+
