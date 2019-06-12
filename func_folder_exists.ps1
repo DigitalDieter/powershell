278231739ff18ec685c4b3e17a17a9d1434a2d0d
Michael <m.mueller444@gmail.com>
2019-06-12 08:30:05 +0200

    Init file

---
+++ func_folder_exists.ps1
@@ -0,0 +1,15 @@
+﻿$folder2test = 'C:\__NSCLIENT Kunden'
+function check_folder_exists
+{
+    if (Test-Path $folder2test -PathType Container)
+    {
+        "Source folder existing"
+    }
+
+    if (-not(Test-Path $folder2test -PathType Container))
+    {
+        "Source folder not existing"
+    }
+
+}
+check_folder_exists
